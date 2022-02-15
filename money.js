26c4809de0812732a39970d59902d19289edcacf
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-02-16 01:39:19 +0600

    function calculation done

--- money.js
+++ money.js
@@ -1,34 +1,54 @@
 // console.log("check")
 
+function CalculteBalanceExpence() {
+  const Income = document.getElementById("income-amount");
+  const IncomeValue = parseInt(Income.value);
+  // console.log(IncomeValue);
+  const Food = document.getElementById("food-amount");
+  const FoodValue = parseInt(Food.value);
+  const Rent = document.getElementById("rent-amount");
+  const RentValue = parseInt(Rent.value);
+  const Cloth = document.getElementById("cloth-amount");
+  const ClothValue = parseInt(Cloth.value);
+  const TotalExpence = FoodValue + RentValue + ClothValue;
+  const TotalExpenceText = document.getElementById("total-expence");
+  TotalExpenceText.innerText = TotalExpence;
 
+  // console.log(TotalExpence)
 
+  const Balance = IncomeValue - TotalExpence;
+  const BalanceText = document.getElementById("total-balance");
+  BalanceText.innerText = Balance;
 
+  // console.log(Balance)
 
-document.getElementById('calculte').addEventListener('click',function(){
+  Income.value = "";
+  Food.value = "";
+  Rent.value = "";
+  Cloth.value = "";
 
-          const Income=document.getElementById('income-amount');
-          const IncomeValue = parseInt(Income.value) ;
-          // console.log(IncomeValue);
-          const Food=document.getElementById('food-amount')
-          const FoodValue=parseInt(Food.value) ;
-          const Rent =document.getElementById('rent-amount')
-          const RentValue = parseInt(Rent.value)   ;
-          const Cloth=document.getElementById('cloth-amount')
-          const ClothValue=parseInt(Cloth.value)  ;
-          const TotalExpence = FoodValue + RentValue + ClothValue;
-          const TotalExpenceText =document.getElementById('total-expence');
-          TotalExpenceText.innerText =TotalExpence;
+  // console.log('click working')
+}
 
-          
-          // console.log(TotalExpence)
+document.getElementById("calculte").addEventListener("click", function () {
+  CalculteBalanceExpence();
+});
 
-          const Balance = IncomeValue - TotalExpence;
-          const BalanceText =document.getElementById("total-balance");
-          BalanceText.innerText =Balance;
+document.getElementById("after-saving").addEventListener("click", function () {
+  // console.log("non saving");
 
-          // console.log(Balance)
+  const IncomeSave = document.getElementById("income-amount");
 
-          Income.value =""
+  const IncomeSaveValue = parseInt(IncomeSave.value);
+  // console.log(IncomeSaveValue);
+  const SavingAmount = document.getElementById("saving-amount");
+  const SavingAmountValue = parseInt(SavingAmount.value);
 
-          // console.log('click working')
-})
+  const TotalSavings = (IncomeSaveValue * SavingAmountValue) / 100;
+  const TotalSavingsAmount = document.getElementById("total-saving-amout");
+  TotalSavingsAmount.innerText = TotalSavings;
+  SavingAmount.value="";
+  IncomeSave.value="";
+
+  // console.log(TotalSavings);
+});
